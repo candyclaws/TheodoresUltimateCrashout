141400b4b63998f7de26d76612ebec810ed6addc
Eshean.A <aidan@arumainathan.com>
2024-09-22 13:46:41 -0700

    things

--- TheodoresUltimateCrashout/scripts/score_canvas.gd
+++ TheodoresUltimateCrashout/scripts/score_canvas.gd
@@ -3,11 +3,12 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$score.visible = false  # Start off hidden
+	$score.visible = true  # Start off hidden
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	updateScore()
-	add_to_scene()
+	#add_to_scene()
+	pass
 func add_to_scene():
 	var curr_scene = get_tree().current_scene
 	if "cutscene" not in curr_scene.name:
