a4a8781f2cfb8cb2f9f46b918d504caa7c5f4084
Eshean.A <aidan@arumainathan.com>
2024-09-21 22:33:31 -0700

    new eshean hitting physics

--- TheodoresUltimateCrashout/scripts/player.gd
+++ TheodoresUltimateCrashout/scripts/player.gd
@@ -1,9 +1,72 @@
+#extends CharacterBody2D
+#
+#
+#const SPEED = 30000.0
+#@onready var animated_sprite_2d = $AnimatedSprite2D
+#var is_attacking = false;
+#
+#@onready var ray_cast_right: RayCast2D = $RayCastRight
+#@onready var ray_cast_left: RayCast2D = $RayCastLeft
+#@onready var ray_cast_down: RayCast2D = $RayCastDown
+#@onready var ray_cast_up: RayCast2D = $RayCastUp
+#
+#
+#func _physics_process(delta: float) -> void:
+	#if is_attacking:
+		#animated_sprite_2d.play("attack")
+		#bat_collision()
+		#return
+	#var horizontal_axis = Input.get_axis("left", "right")
+	#var vertical_axis = Input.get_axis("forward", "down") 
+	#var direction = Vector2(horizontal_axis, vertical_axis)
+	#if Input.is_action_just_pressed("attack"):
+		#is_attacking = true;
+		#$Timer.start()
+	#else:
+			 ## Using 'await' to play animation fully
+		#if direction != Vector2.ZERO:
+			#direction = direction.normalized()
+		#velocity = direction*SPEED*delta
+		#if(velocity < Vector2(0, 0)):
+			#animated_sprite_2d.flip_h = true;
+			#animated_sprite_2d.play("walking")
+		#elif(velocity > Vector2(0, 0)):
+			#animated_sprite_2d.flip_h = false;
+			#animated_sprite_2d.play("walking")
+		#else:
+			#animated_sprite_2d.play("idle")
+	#move_and_slide()
+	#
+	#
+#
+#
+#func _on_timer_timeout() -> void:
+	#is_attacking = false; 
+	#
+#func bat_collision():
+	#if(ray_cast_right.is_colliding() && ray_cast_right.get_collider() is RigidBody2D):
+		#ray_cast_right.get_collider().got_hit(1)
+	#elif(ray_cast_left.is_colliding() && ray_cast_left.get_collider() is RigidBody2D):
+		#ray_cast_left.get_collider().got_hit(2)
+	#elif(ray_cast_up.is_colliding() && ray_cast_up.get_collider() is RigidBody2D):
+		#ray_cast_up.get_collider().got_hit(3)
+	#elif(ray_cast_down.is_colliding() && ray_cast_down.get_collider() is RigidBody2D):
+		#ray_cast_down.get_collider().got_hit(4)
+
 extends CharacterBody2D
 
 
 const SPEED = 30000.0
 @onready var animated_sprite_2d = $AnimatedSprite2D
 var is_attacking = false;
+
+
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_down: RayCast2D = $RayCastDown
+@onready var ray_cast_up: RayCast2D = $RayCastUp
+
 
 func _physics_process(delta: float) -> void:
 	if is_attacking:
@@ -22,10 +85,10 @@ func _physics_process(delta: float) -> void:
 		velocity = direction*SPEED*delta
 		if(velocity < Vector2(0, 0)):
 			animated_sprite_2d.flip_h = true;
-			animated_sprite_2d.play("walk")
+			animated_sprite_2d.play("walking")
 		elif(velocity > Vector2(0, 0)):
 			animated_sprite_2d.flip_h = false;
-			animated_sprite_2d.play("walk")
+			animated_sprite_2d.play("walking")
 		else:
 			animated_sprite_2d.play("idle")
 	move_and_slide()
@@ -34,4 +97,16 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
+	bat_collision()
 	is_attacking = false; 
+	
+func bat_collision():
+	if(ray_cast_right.is_colliding() && ray_cast_right.get_collider() is RigidBody2D):
+		ray_cast_right.get_collider().got_hit(1)
+	elif(ray_cast_left.is_colliding() && ray_cast_left.get_collider() is RigidBody2D):
+		ray_cast_left.get_collider().got_hit(2)
+	elif(ray_cast_up.is_colliding() && ray_cast_up.get_collider() is RigidBody2D):
+		ray_cast_up.get_collider().got_hit(3)
+	elif(ray_cast_down.is_colliding() && ray_cast_down.get_collider() is RigidBody2D):
+		ray_cast_down.get_collider().got_hit(4)
+	
--- TheodoresUltimateCrashout/scripts/vase.gd
+++ TheodoresUltimateCrashout/scripts/vase.gd
@@ -2,15 +2,35 @@ extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
-var initial_velocity = 5000
+var initial_velocity = 0;
 var target_velocity = 0.0
 var slowdown = 0.05
+var x = -1;
 func _ready() -> void:
 	linear_velocity.x = initial_velocity # Replace with function body.
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	linear_velocity.x = lerp(linear_velocity.x, target_velocity, slowdown)
+	var factor = 1;
+	if(linear_velocity.x < 0 || linear_velocity.y < 0):
+		factor = -1
+	if(x == -1):
+		return
+	else:
+		if(x <= 2):
+			linear_velocity.x = lerp(linear_velocity.x, target_velocity, slowdown*factor)
+		else:
+			linear_velocity.y = lerp(linear_velocity.y, target_velocity, slowdown*factor)
 func got_hit(z: float):
-	pass
+	initial_velocity = 3000;
+	x= z;
+	if(z == 1):
+		linear_velocity.x = initial_velocity
+	elif(z==2):
+		linear_velocity.x = -initial_velocity
+	elif(z == 3):
+		linear_velocity.y = -initial_velocity
+	elif(z==4):
+		linear_velocity.y =  initial_velocity
+		
